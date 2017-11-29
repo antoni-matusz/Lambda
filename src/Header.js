4e31bcd8d63eb35db18759c4da3a0052612a1724
Aleksandra Weber <hello.aleksandra-weber.pl>
2017-11-30 00:54:21 +0100

    second

--- src/Header.js
+++ src/Header.js
@@ -6,8 +6,8 @@ class Header extends Component {
       <div className='header-bg'>
         <div className='container'>
           <div className='row'>
-            <img src ='/images/logo.png' className='col-md-2'/>
-            <ul className='col-md-9'>
+            <img src='/images/logo.png' className='col-md-2 logo' alt='logo'/>
+            <ul className='col-md-7'>
               <li>Home</li>
               <li>About</li>
               <li>Igredients</li>
@@ -15,11 +15,25 @@ class Header extends Component {
               <li>Reviews</li>
               <li>Reservations</li>
             </ul>
-            <div className='col-md-1 icons-box'>
+            <div className='col-md-3 icons-box'>
+            <i className="fa fa-twitter" aria-hidden="true"></i>
+            <i className="fa fa-youtube" aria-hidden="true"></i>
+            <i className="fa fa-facebook" aria-hidden="true"></i>
             </div>
           </div>
           <div className='row'>
-            <div className='col-md-12'>the right ingredients for the right food</div>
+            <h1 className='col-md-12'>the right ingredients for the right food</h1>
+          </div>
+          <div className='row'>
+            <img src='/images/devider.png'className='col-md-12 devider' alt='devider'/>
+          </div>
+          <div className='row'>
+            <div className='col-md-6'>
+              <button>book a table</button>
+            </div>
+            <div className='col-md-6'>
+            <button>see the menu</button>
+            </div>
           </div>
         </div>
       </div>
